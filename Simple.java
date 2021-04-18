898dd638e4b7aa08a65ec6de306e1afcf23ac97d
sajidsk <sajidshaik@live.com>
2021-04-18 14:59:07 +0530

    Create Simple.java

---
+++ Simple.java
@@ -0,0 +1,9 @@
+class class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+    }  
+}  {  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+    }  
+}  
