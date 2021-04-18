bebaa2d905a14ea7d69aa758a9ef0a96ee36406d
sajidsk <sajidshaik@live.com>
2021-04-18 15:08:06 +0530

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -2,8 +2,5 @@ class class Simple{
     public static void main(String args[]){  
      System.out.println("Hello Java");  
     }  
-}  {  
-    public static void main(String args[]){  
-     System.out.println("Hello Java");  
-    }  
+}  
 }  
